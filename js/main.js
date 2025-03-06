c6ab330bbc094b83a9c62f6acd4e8962461124bf
Laura <laura.laukiz@gmail.com>
2025-03-06 11:22:49 +0100

    ejercicio 2.11

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 'use strict'
+
+const taskList = document.querySelector(".js-li");
+
+const tasks = [
+    { name: "Recoger setas en el campo", completed: true, id: 1 },
+    { name: "Comprar pilas", completed: true, id: 2 },
+    { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+    {
+      name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+      completed: false,
+      id: 4,
+    },
+  ];
+
+  for (const task of tasks) {
+    // pintar la tarea en la lista
+
+    if (task.completed === true) {
+        taskList.innerHTML += `<li class="lista cross-out" id="${task.id}">${task.name}</li>`
+        } else {
+            taskList.innerHTML += `<li class="lista" id="${task.id}">${task.name}</li>`
+        }
+
+    
+    }
+
+
+
+/*     for  (let i=0; i<tasks.length; i++) {
+        taskList.innerHTML += `<li class="lista">${tasks[i].name}</li>`
+    } */
